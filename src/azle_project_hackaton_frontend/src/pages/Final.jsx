9ae3e86944b335950093af78ef9988d402d06328
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-07 14:06:32 -0600

    feat: fetch data from API

--- src/azle_project_hackaton_frontend/src/pages/Final.jsx
+++ src/azle_project_hackaton_frontend/src/pages/Final.jsx
@@ -1,46 +1,62 @@
 import SampleRecord from "../components/SampleRecord";
+import Alert from "../components/Alert";
+import { useEffect, useState } from "react";
+import { useAuth, useRestActor } from "@bundly/ares-react";
 
 const Final = () => {
-  const samples = [
-    {
-      id: 1,
-      dateTime: '2024-07-07 14:30:00',
-      tds: '150 ppm',
-      temperature: '29°C',
-      pH: '7.5',
-      turbidity: '5 NTU',
-      quantity: '100 liters',
-    },
-    {
-      id: 2,
-      dateTime: '2024-07-07 15:30:00',
-      tds: '160 ppm',
-      temperature: '26°C',
-      pH: '7.4',
-      turbidity: '6 NTU',
-      quantity: '110 liters',
-    },
-    {
-      id: 3,
-      dateTime: '2024-07-07 16:30:00',
-      tds: '170 ppm',
-      temperature: '27°C',
-      pH: '7.3',
-      turbidity: '7 NTU',
-      quantity: '120 liters',
-    },
-    // Add more sample data as needed
-  ];
+
+  const backend = useRestActor('azle_project_hackaton_backend');
+
+  const { isAuthenticated } = useAuth();
+
+  const [alert, setAlert] = useState(null);
+  const [samples, setSamples] = useState([]);
+
+  useEffect(() => {
+    async function validate(){
+      if (!isAuthenticated) {
+        setAlert({
+          type: 'alert',
+          message: 'You need to be authenticated to see this page',
+        });
+        return;
+      }
+      try {
+        const response = await backend.get('final_samples');
+        setSamples(response.data.message);
+      } catch (error) {
+        setAlert({
+          type: 'error',
+          message: 'An error occurred while fetching the data',
+        });
+      }
+      if (samples.length === 0) {
+        setAlert({
+          type: 'alert',
+          message: 'No samples found',
+        });
+        return;
+      }
+      setAlert(null);
+    }
+    validate();
+  }, [samples.length, isAuthenticated]);
 
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <div className="max-w-6xl mx-auto">
         <h1 className="text-2xl font-bold text-blue-800 mb-4 text-center">Final Tests History</h1>
-        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {samples.map(sample => (
-            <SampleRecord key={sample.id} data={sample} />
-          ))}
-        </div>
+        {
+          samples.length === 0 ? (
+            <Alert data={alert} />
+          ) : (
+            <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {samples.map(sample => (
+                <SampleRecord key={sample.id} data={sample} />
+              ))}
+            </div>
+          )
+        }
       </div>
     </div>
   );
