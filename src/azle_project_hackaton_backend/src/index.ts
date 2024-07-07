9b14efc5d5e0e05af9e5c3c77758efcce8e0755f
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-07 14:06:54 -0600

    feat: create endpoint to get data

--- src/azle_project_hackaton_backend/src/index.ts
+++ src/azle_project_hackaton_backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Server, ic } from 'azle';
+import { Server } from 'azle';
 import express from 'express';
 import cors from 'cors';
 
@@ -6,35 +6,35 @@ export default Server(() => {
 
     type Sample = {
         id: String,
-        date: Date,
-        ph: Number,
+        dateTime: Date,
+        pH: Number,
         temperature: Number,
         turbidity: Number,
         tds: Number,
-        amount: Number
+        quantity: Number
     };
 
     let initial_sample : Sample[] = [
         {
             id: "872910203810",
-            date: new Date('2024-07-27T16:56:00'),
-            ph: 7.8,
+            dateTime: new Date('2024-07-27T16:56:00'),
+            pH: 7.8,
             temperature: 23.5,
             turbidity: 430,
             tds: 230,
-            amount: 23.7
+            quantity: 23.7
         }
     ];
 
     let final_sample : Sample[] = [
         {
             id: "872910203402",
-            date: new Date('2024-07-27T17:34:00'),
-            ph: 8.7,
+            dateTime: new Date('2024-07-27T17:34:00'),
+            pH: 8.7,
             temperature: 23.5,
             turbidity: 215,
             tds: 120,
-            amount: 19.9
+            quantity: 19.9
         }
     ];
 
@@ -43,15 +43,26 @@ export default Server(() => {
     app.use(cors());
     app.use(express.json());
 
-    // app.use((req, res, next) => {
-    //     if (ic.caller().isAnonymous()) {
-    //         return res.status(401).send({message: 'Unauthorized. Please login to access this resource'});
-    //     }
-    //     next();
-    // });
-
     app.get('/greet', (req: Request, res: any) => {
-        return res.status(200).send({message: 'Hello World from Azle!', caller: ic.caller()});
+        return res.status(200).send({message: 'Hello World from Azle!'});
+    });
+
+    app.get('/initial_samples', (req: Request, res: any) => {
+        return res.status(200).send({message: initial_sample});
+    });
+
+    app.get('/final_samples', (req: Request, res: any) => {
+        return res.status(200).send({message: final_sample});
+    });
+
+    // Get the latest initial sample
+    app.get('/latest_initial_sample', (req: Request, res: any) => {
+        return res.status(200).send({message: initial_sample[initial_sample.length - 1]});
+    });
+
+    // Get the latest final sample
+    app.get('/latest_final_sample', (req: Request, res: any) => {
+        return res.status(200).send({message: final_sample[final_sample.length - 1]});
     });
 
     return app.listen();
