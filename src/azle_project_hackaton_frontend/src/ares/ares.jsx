cf34e47146a17531ecabaefa714cea48f615ae18
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-07 18:08:36 -0600

    remove file

--- src/azle_project_hackaton_frontend/src/ares/ares.jsx
+++
@@ -1,8 +0,0 @@
-import ares from '@bundly/ares-rest';
-
-const aresInstance = ares.create({
-    // baseURL: `${process.env.NEXT_PUBLIC_API_REST_URL}`,
-    baseURL: 'http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:4943',
-});
-
-export default aresInstance;
