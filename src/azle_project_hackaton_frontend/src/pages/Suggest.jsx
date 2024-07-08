a6041edc1f9707c42ae82bb97230930f8e514181
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-07 19:26:21 -0600

    feat: show water quality suggestions

--- src/azle_project_hackaton_frontend/src/pages/Suggest.jsx
+++ src/azle_project_hackaton_frontend/src/pages/Suggest.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import waterQualityData from '../mocks/data.js';
+import InfoCard from '../components/InfoCard.jsx';
 
-const Suggest = () => {
+const Suggests = () => {
   return (
-    <div>Suggest</div>
-  )
+      <div className="p-6 flex flex-col items-center">
+          <h1 className="text-4xl font-bold text-blue-900 mb-8">Water Quality Suggestions</h1>
+          {waterQualityData.map((data, index) => (
+              <InfoCard key={index} data={data} />
+          ))}
+      </div>
+  );
 }
 
-export default Suggest
+export default Suggests;
