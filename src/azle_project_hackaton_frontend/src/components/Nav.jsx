16a1974b1a165fc9b22a3965e41786a21fd50a85
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-10 10:23:39 -0600

    feat: make navigation header responsive

--- src/azle_project_hackaton_frontend/src/components/Nav.jsx
+++ src/azle_project_hackaton_frontend/src/components/Nav.jsx
@@ -1,6 +1,5 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { InternetIdentityButton, useAuth, LogoutButton } from "@bundly/ares-react";
-
 
 const Nav = () => {
 
@@ -11,10 +10,10 @@ const Nav = () => {
         <div className="w-4/5 my-auto">
             <ul className="flex justify-evenly font-bold text-3xl text-blue-900">
                 <li><Link to='/'><img src="./aquanova.png" alt="AquaNova Home" className="w-24" /></Link></li>
-                <li className="my-auto"><Link to='/initial'>Initial Test</Link></li>
-                <li className="my-auto"><Link to='/final'>Final Test</Link></li>
-                <li className="my-auto"><Link to='/test'>Test</Link></li>
-                <li className="my-auto"><Link to='/suggest'>Suggestions</Link></li>
+                <li className="lg:my-auto hidden lg:block"><Link to='/initial'>Initial Test</Link></li>
+                <li className="lg:my-auto hidden lg:block"><Link to='/final'>Final Test</Link></li>
+                <li className="lg:my-auto hidden lg:block"><Link to='/test'>Test</Link></li>
+                <li className="lg:my-auto hidden lg:block"><Link to='/suggest'>Suggestions</Link></li>
             </ul>
         </div>
         <div className="flex w-1/5 justify-center">
@@ -28,4 +27,4 @@ const Nav = () => {
   )
 }
 
-export default Nav
+export default Nav;
